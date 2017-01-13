f7280b86afbc12bfc5a1450145b99a96b4a34cf5
MPawluk <m.blizniak@gmail.com>
2017-01-13 23:11:37 +0100

    Some help :)

--- src/main/resources/static/app/scripts/app.js
+++ src/main/resources/static/app/scripts/app.js
@@ -3,9 +3,29 @@
  */
 (function () {
     "use strict";
-    var app = angular.module("theGame", []);
+    var app = angular.module("theGame", ['ui.bootstrap']);
+
+
     app.controller("firstController", function() {
-       this.someVar = "zmieniam jej wartoœæ!";
+       this.someVar = "zmieniam jej wartoï¿½ï¿½!";
+    });
+
+    app.controller("backendController", function($http) {
+        var vm = this;
+        
+        vm.players = [];
+        vm.getPlayers = getPlayers;
+
+        function getPlayers() {
+            $http.get("players").then(
+                function success(playerData) {
+                    angular.copy(playerData.data, vm.players);
+                }, 
+                function failure() {
+                    alert("Players are not here :(");
+                }
+            );
+        }
     });
 }());
 
